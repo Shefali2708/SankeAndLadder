0ddf74364062c2fda643edc4f17c33f8c60c057d
shefali <shefalis2708@gmail.com>
2022-04-02 19:13:19 +0530

    NoPlay,Snake,Ladder

--- Program.cs
+++ Program.cs
@@ -8,14 +8,12 @@ namespace SnakeAndLadder
         {
             Console.WriteLine(" Hello to Snake And Ladder Game !");
 
-            // Snake and Ladder game played with single player at start position 0
-
             SLGameUC1 GameUC1 = new SLGameUC1();
             GameUC1.Start_Game();
 
             SLGameUC2.Start_Game();
 
-
+            SLGameUC3.Start_Game();
         }
     }
 }
